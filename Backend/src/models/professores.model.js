23f5ed9c8980fee47bd1f38a8064ac6a09ee5a71
SamuelRicosta <90395032+SamuelRicosta@users.noreply.github.com>
2023-10-29 14:20:20 -0300

    busca pela letra

--- Backend/src/models/professores.model.js
+++ Backend/src/models/professores.model.js
@@ -12,7 +12,7 @@ const getAllProfessors = async () => db.collection('professores').find().toArray
 
 const findProfessorByName = async (nome) => {
   try {
-    const professores = await db.collection('professores').find({ nome: new RegExp(nome, 'i') }).toArray();
+    const professores = await db.collection('professores').find({ nome: new RegExp('^' + nome, 'i') }).toArray();
     return professores;
   } catch (error) {
     return [];

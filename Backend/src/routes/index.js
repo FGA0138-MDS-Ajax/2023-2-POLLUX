3104369a1d15781ce9902ecc91a82cff8d64bb5f
SamuelRicosta <90395032+SamuelRicosta@users.noreply.github.com>
2023-11-13 19:40:10 -0300

    add professor

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,8 +3,8 @@ import { requestLogin } from '../models/usuario.model';
 import {
   getAll, createUser, deleteUser, updateUser,
 } from '../controllers/usuariocontroller';
-import { getAllProfessors, findProfessorByName } from '../models/professores.model';
-
+import { getAllProfessors, findProfessorByName, adicionarComentario } from '../models/professores.model';
+import { findUserById } from '../models/usuario.model';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
@@ -26,6 +26,22 @@ routes.get('/professores/search', async (req, res) => {
   const { nome } = req.query;
   const professores = await findProfessorByName(nome);
   res.send(professores);
+});
+
+routes.get('/usuarios/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await findUserById(id);
+  res.send(user);
+});
+
+routes.post('/comentarios', async (req, res) => {
+  const { professorId, texto } = req.body;
+  const result = await adicionarComentario(professorId, texto);
+  if (result) {
+    res.status(200).send({ success: 'Comentário adicionado com sucesso' });
+  } else {
+    res.status(500).send({ error: 'Erro ao adicionar comentário' });
+  }
 });
 
 export default routes;

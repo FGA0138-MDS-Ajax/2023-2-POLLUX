130670a6fe262151e503486ab01bc4f0370c0c3a
eric <eric.rabelo21@gmail.com>
2023-11-13 19:31:33 -0300

    feat/criando sessão de avaliações

---
+++ Frontend/src/components/Review/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//styles
+import './styles.css'
+
+function Review() {
+  return (
+    <div className='review-wrapper'>
+        
+    </div>
+  );
+}
+
+export default Review;

80bf0aed98f03fddef1f844773c1b62cc1a8a04a
eric <eric.rabelo21@gmail.com>
2023-11-30 21:51:43 -0300

    validação do email

--- Frontend/src/pages/SucessValidation/index.js
+++ Frontend/src/pages/SucessValidation/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+//styles
+import "./styles.css";
+import sucess from "../../assets/images/sucess.png"
 
 function SucessValidation () {
     return (
-        <div>
-            <h1>Conta verificada com sucesso!</h1>
-            <p>Você já pode fechar essa página!!</p>
+        <div className="sucess-container">
+            <h1>Parabéns! Sua conta verificada com sucesso!</h1>
+            <img src={sucess} className="sucess-image"></img>
+            <p>Você já pode fechar essa página agora.</p>
         </div>
     );
 }
